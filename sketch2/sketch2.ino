48c17c55518b27b911f0d447a5bf7725968d24b4
Patrick James Mardis <59069682+patrickjamesmardis@users.noreply.github.com>
2020-10-08 18:12:33 -0400

    init

--- sketch2/sketch2.ino
+++ sketch2/sketch2.ino
@@ -1,21 +1,17 @@
+//include and initialize servo
 #include <Servo.h>
 Servo myservo;
 
 void setup() {
+  //using pin 9 for servo
   myservo.attach(9);
-  Serial.begin(9600);
 }
 
 void loop() {
-
-
+  //map the photocell's input from 0-1023 range to servo's 0-180 range
   float pos = map(analogRead(0), 0, 1023, 0, 180);
-  
-  myservo.write(pos);
-  Serial.print(pos);
-  Serial.print(" = light: ");
-  Serial.println(analogRead(0));
-  delay(250);
 
+  //update servo position
+  myservo.write(pos);
 
 }
